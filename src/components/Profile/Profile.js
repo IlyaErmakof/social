a966c4bf818f4bce6107ee303691f10fc3417261
Ilya <ilaermakov@MacBook-Air-Ila.local>
2022-04-04 14:07:09 +0300

    creat components

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,9 @@
+const  Profile = () => {
+    return (
+        <div>
+            rjyntyn
+        </div>
+        );
+    }
+
+export default Profile;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,57 @@
+const  SideBar = () => {
+  return (
+    <div className="sidebar">
+        <div className="sidebar-header">
+        <a href="#">
+            <img src="http://demo.foxthemes.net/instellohtml/assets/images/logo.png" class="logo_inverse" />
+        </a>
+        </div>
+        <div className="sidebar-inner">
+            <div className="profile-content">
+                <div className="profile-icon">
+                    <img src="http://demo.foxthemes.net/instellohtml/assets/images/avatars/avatar-2.jpg" />
+                </div>
+                <div className="profile-title">
+                    <a href="#">Ilya Ermakov</a>
+                </div>
+                <div className="profile-info">
+                    <div>
+                        <span>Post</span>
+                        <span className="profile-info-count">132</span>
+                    </div>
+                    <div>
+                        <span>Following</span>
+                        <span className="profile-info-count">1,132</span>
+                    </div>
+                    <div>
+                        <span>Followers</span>
+                        <span className="profile-info-count">23,222</span>
+                    </div>
+                </div>
+            </div>
+            <div className="profile-navbar">
+                <ul>
+                    <li>My Profile</li>
+                    <li>Feed</li>
+                    <li>Messages</li>
+                    <li>Settings</li>
+                    <li>Settings</li>
+                    <li>Settings</li>
+                    <li>Settings</li>
+                    <li>Settings</li>
+                </ul>
+            </div>
+            <div className="profile-logout">
+                <ul>
+                    <li>
+                        <a href="#">Logout</a>
+                    </li>
+                </ul>
+               
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default SideBar;
