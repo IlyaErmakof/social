cffaa4f681b9c22cdae733f2beb978b18fdd5526
Ilya Ermakov <ilya.ermakov01gmail.com>
2022-04-16 18:33:20 +0300

    Add profile jsx

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import './App.css';
-import MainContent from './components/MainContent';
-import SideBar from './components/Sidebar';
+import "./App.css";
+import MainContent from "./components/MainContent";
+import SideBar from "./components/Sidebar";
 
-function proper(world){
-  world.toLowerCase()
-  let ab = []
-  for(let i = world.length; i>=0; i--)
-  {
-    ab.push(world[i])
+function proper(world) {
+  world.toLowerCase();
+  let ab = [];
+  for (let i = world.length; i >= 0; i--) {
+    ab.push(world[i]);
   }
-  let resilt = ab.join('')
-  return resilt
+  let resilt = ab.join("");
+  return resilt;
 }
-console.log(proper('ava98saa'))
+console.log(proper("ava98saa"));
 
 function App() {
   return (
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,56 +1,72 @@
-const  SideBar = () => {
+const SideBar = () => {
   return (
     <div className="sidebar">
-        <div className="sidebar-header">
+      <div className="sidebar-header">
         <a href="#">
-            <img src="http://demo.foxthemes.net/instellohtml/assets/images/logo.png" class="logo_inverse" />
+          <img
+            src="http://demo.foxthemes.net/instellohtml/assets/images/logo.png"
+            class="logo_inverse"
+          />
         </a>
+      </div>
+      <div className="sidebar-inner">
+        <div className="profile-content">
+          <div className="profile-icon">
+            <img src="http://demo.foxthemes.net/instellohtml/assets/images/avatars/avatar-2.jpg" />
+          </div>
+          <div className="profile-title">
+            <a href="#">Ilya Ermakov</a>
+          </div>
+          <div className="profile-info">
+            <div>
+              <span>Post</span>
+              <span className="profile-info-count">132</span>
+            </div>
+            <div>
+              <span>Following</span>
+              <span className="profile-info-count">1,132</span>
+            </div>
+            <div>
+              <span>Followers</span>
+              <span className="profile-info-count">23,222</span>
+            </div>
+          </div>
         </div>
-        <div className="sidebar-inner">
-            <div className="profile-content">
-                <div className="profile-icon">
-                    <img src="http://demo.foxthemes.net/instellohtml/assets/images/avatars/avatar-2.jpg" />
-                </div>
-                <div className="profile-title">
-                    <a href="#">Ilya Ermakov</a>
-                </div>
-                <div className="profile-info">
-                    <div>
-                        <span>Post</span>
-                        <span className="profile-info-count">132</span>
-                    </div>
-                    <div>
-                        <span>Following</span>
-                        <span className="profile-info-count">1,132</span>
-                    </div>
-                    <div>
-                        <span>Followers</span>
-                        <span className="profile-info-count">23,222</span>
-                    </div>
-                </div>
-            </div>
-            <div className="profile-navbar">
-                <ul>
-                    <li><a href="#">My Profile</a></li>
-                    <li><a href="#">Feed</a></li>
-                    <li><a href="#">Messages</a></li>
-                    <li><a href="#">Explore</a></li>
-                    <li><a href="#">Trending</a></li>
-                    <li><a href="#">Marketplace</a></li>
-                    <li><a href="#">Settings</a></li>
-                </ul>
-            </div>
-            <div className="profile-logout">
-                <ul>
-                    <li>
-                        <a href="#">Logout</a>
-                    </li>
-                </ul>
-               
-            </div>
+        <div className="profile-navbar">
+          <ul>
+            <li>
+              <a href="#">My Profile</a>
+            </li>
+            <li>
+              <a href="#">Feed</a>
+            </li>
+            <li>
+              <a href="#">Messages</a>
+            </li>
+            <li>
+              <a href="#">Explore</a>
+            </li>
+            <li>
+              <a href="#">Trending</a>
+            </li>
+            <li>
+              <a href="#">Marketplace</a>
+            </li>
+            <li>
+              <a href="#">Settings</a>
+            </li>
+          </ul>
         </div>
+        <div className="profile-logout">
+          <ul>
+            <li>
+              <a href="#">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default SideBar;
